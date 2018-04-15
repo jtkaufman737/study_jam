4d288ebe5c0ddc85b5c455bad3f65f0999e82483
Joy <jtkaufman737@gmail.com>
2018-04-15 13:41:08 -0400

    Main.js initial setup add app component to index.html

--- study-jam/src/main.js
+++ study-jam/src/main.js
@@ -1,15 +1,34 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import App from './App.vue'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 
-Vue.config.productionTip = false
+Vue.use(VueResource)
+Vue.use(VueRouter)
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+const router = new VueRouter()
+
+router.map({
+  '/hello': {
+    component: Hello
+  }
 })
+
+router.redirect({
+  // Any misguided traffic gets sent back home
+  '*': '/hello'
+})
+
+router.start(App,'#app')
+
+// Vue.config.productionTip = false
+//
+// /* eslint-disable no-new */
+// new Vue({
+//   el: '#app',
+//   router,
+//   components: { App },
+//   template: '<App/>'
+// })
